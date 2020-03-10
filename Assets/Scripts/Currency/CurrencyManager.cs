de6f6d51d97991271f5babfb9d75ee7a8c219229
samjerry <jerry.sam@ziggo.nl>
2020-03-10 13:42:53 +0100

    Added CurrencyManager Script

---
+++ Assets/Scripts/Currency/CurrencyManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyManager : MonoBehaviour
+{
+    private int _moneyz;
+
+    void OnMoneyzChange(int amount)
+    {
+        _moneyz += amount;
+    }
+}
