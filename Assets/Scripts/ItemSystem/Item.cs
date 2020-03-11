043bc1a0eb09655a5425f4d213848044bfc0e3fb
Timo Heijne <me@timoheijne.nl>
2020-03-11 11:53:49 +0100

    Add basic item system

---
+++ Assets/Scripts/ItemSystem/Item.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ItemSystem
+{
+    [CreateAssetMenu(menuName = "Grid/Item", order = 0)]
+    public class Item : ScriptableObject
+    {
+        public int UniqueItemId;
+        public string ItemName;
+        public bool IsActive;
+        public GameObject ItemPrefab;
+    }
+}
---
+++ Assets/Scripts/ItemSystem/ItemSystem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ItemSystem
+{
+    public class ItemSystem : MonoBehaviour
+    {
+        private static ItemSystem _instance;
+
+        public static ItemSystem Instance
+        {
+            get { return _instance; }
+        }
+
+        private List<Item> items;
+
+        private void Start()
+        {
+            if (ItemSystem.Instance == null)
+            {
+                _instance = this;
+                loadItems("Grid Nodes");
+            }
+            else
+            {
+                Debug.LogError("ItemSystem already exists.");
+                Destroy(this);
+            }
+        }
+
+        private void loadItems(string path)
+        {
+            items = Resources.LoadAll<Item>(path).ToList();
+
+            foreach (Item item in items)
+            {
+                List<Item> itemDuplicateCheck = items.FindAll(i => i.UniqueItemId == item.UniqueItemId).ToList();
+
+                if (itemDuplicateCheck.Count > 1)
+                {
+                    Debug.LogError($"ItemID {item.UniqueItemId} exists {itemDuplicateCheck.Count} times (found on item: {item.ItemName})");
+                }
+            }
+        }
+ 
+        public Item GetItemByID(int itemId)
+        {
+            return items.Find(i => i.UniqueItemId == itemId);
+        }
+    } 
+}
