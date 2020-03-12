1d8acaf1265374390283855622c6a7aa321d5719
Timo Heijne <me@timoheijne.nl>
2020-03-12 11:13:40 +0100

    Add basic grid filling

---
+++ Assets/Scripts/Grid/GridNode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using ItemSystem;
+
+namespace Grid
+{
+    public enum TileType
+    {
+        BaseTile,
+        DecoratorTile,
+        ItemTile,
+    }
+    public class GridNode: MonoBehaviour
+    {
+        // The following is for serializing and storing grid data
+        public int BaseTileId;
+        public int DecoratorTileId;
+        public int ItemTileId;
+
+        // Following is for the game play
+        private GameObject _baseTile;
+        private GameObject _decoratorTile;
+        private GameObject _itemTile;
+
+        public void SetTile(TileType tileType, int itemID)
+        {
+            Item item = ItemSystem.ItemSystem.Instance.GetItemByID(itemID);
+            if (item == null || item.ItemPrefab == null) return;
+            
+            switch (tileType)
+            {
+                case TileType.BaseTile:
+                    BaseTileId = itemID;
+                    if(_baseTile != null)
+                        Destroy(_baseTile);
+                    _baseTile = Instantiate(item.ItemPrefab, transform);
+                    break;
+                case TileType.DecoratorTile:
+                    DecoratorTileId = itemID;
+                    if(_decoratorTile != null)
+                        Destroy(_baseTile);
+                    _decoratorTile = Instantiate(item.ItemPrefab, transform);
+                    break;
+                case TileType.ItemTile:
+                    ItemTileId = itemID;
+                    if(_itemTile != null)
+                        Destroy(_baseTile);
+                    _itemTile = Instantiate(item.ItemPrefab, transform);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(tileType), tileType, null);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Grid/GridSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+namespace Grid
+{
+    public class GridSystem : MonoBehaviour
+    {
+        [Tooltip("Define the size of the grid, X*Y")]
+        public Vector2Int GridSize;
+
+        [Tooltip("Define the size of each tile in unity units")]
+        public Vector2 TileSize = new Vector2(1, 1);
+
+        [Tooltip("The base node every node should have")]
+        public GameObject NodePrefab;
+        
+        private GridNode[,] _grid;
+        
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Initialize grid with grid size
+            
+            fillGrid();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void fillGrid()
+        {
+            _grid = new GridNode[GridSize.x, GridSize.y];
+
+            for (int i = 0; i < GridSize.x; i++)
+            {
+                for (int j = 0; j <  GridSize.y; j++)
+                {
+                    GameObject go = Instantiate(NodePrefab, transform);
+                    go.gameObject.name = $"Node {i}x{j}";
+                    go.transform.position = new Vector3(TileSize.x * i,0,TileSize.y * j);
+                    
+                    GridNode node = go.GetComponent<GridNode>();
+                    node.SetTile(TileType.BaseTile, node.BaseTileId);
+                    _grid[i, j] = node;
+                }
+            }
+        }
+    }
+}
