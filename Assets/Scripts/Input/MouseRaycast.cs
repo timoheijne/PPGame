e67374cffd72a586efc89ac1747cf57bd1facef9
Timo Heijne <me@timoheijne.nl>
2020-03-12 11:15:44 +0100

    Add a very basic mouse to raycast -> grid click

---
+++ Assets/Scripts/Input/MouseRaycast.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Input
+{
+    public class MouseRaycast : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (UnityEngine.Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition);
+                
+                int layer_mask = LayerMask.GetMask("SelectionLayer");
+                if (Physics.Raycast(ray, out hit, 1000, layer_mask)) {
+                    Transform objectHit = hit.transform;
+                    Debug.Log(objectHit.name);
+                    // Do something with the object that was hit by the raycast.
+                }
+            }
+        }
+    }
+}
