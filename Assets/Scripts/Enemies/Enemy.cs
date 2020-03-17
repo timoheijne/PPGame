db1236f989e5d0433e0dd3ac4253ce57d6da78ae
MarrowOfAlbion <jerry.sam@ziggo.nl>
2020-03-17 11:34:29 +0100

    Add base Enemy script

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour {
+
+    protected float damageAmount = 10f;   
+    public float hitPoints = 100f;        
+    public float moveRate = 10f;        
+
+    //public PlayerHealth playerHealth;
+    public Animator anim;
+
+    public void Start() {
+        //playerHealth = GameObject.Find("Player").GetComponent<PlayerHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+    public void setdamageAmount(int a) 
+    {
+        damageAmount = a;
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyTypes/Footman.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footman : Enemy 
+{
+    private int _power = 10;
+  
+        void Start() {
+        base.Start();
+        setdamageAmount(power);
+        // other unique start stuff here...
+    }
+
+    void Update() {
+        //do things an enemy child would
+    }
+}
