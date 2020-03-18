11b629d1b3ac481db4cea09f9d019e0570c70525
neocccro <cavandiepen@gmail.com>
2020-03-18 11:44:34 +0100

    Add health system

---
+++ Assets/Dwemo Scenes/Health/HealthDemo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDemo : MonoBehaviour
+{
+    [SerializeField] GameObject healthBar;
+    Health health;
+    float healthBarY;
+    float healthBarZ;
+
+
+    void Start()
+    {
+        healthBarY = healthBar.transform.localScale.y;
+        healthBarZ = healthBar.transform.localScale.z;
+
+        health = GetComponent<Health>();
+        health.onHealthChange += updateBar;
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            health.doHeal(1);
+            Debug.Log("heal 1");
+        }
+        else
+        {
+            health.doDamage(1);
+            Debug.Log("damage 1");
+        }
+    }
+
+    private void updateBar(float currentFraction, float change)
+    {
+        healthBar.transform.localScale = new Vector3(currentFraction, healthBarY, healthBarZ);
+    }
+}
---
+++ Assets/Scwipts/Health.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 0;
+    private float currentHealth = 0;
+    private float armor = 0;
+    private float shield = 0;
+    public delegate void DieHandler();
+    public delegate void HealthChangeHandler(float currentFraction, float change);
+    public DieHandler onDie;
+    public HealthChangeHandler onHealthChange;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public float getHealthFraction()
+    {
+        return currentHealth / maxHealth;
+    }
+
+    public bool doDamage(float damage)
+    {
+        float damageDone = Mathf.Min(damage, currentHealth);
+        currentHealth = Mathf.Max(currentHealth - damage, 0);
+        onHealthChange?.Invoke(getHealthFraction(), Mathf.Min(currentHealth, damageDone));
+
+        if (currentHealth <= 0)
+        {
+            onDie?.Invoke();
+            return true;
+        }
+
+        return false;
+    }
+
+    public void doHeal(float heal)
+    {
+        if (currentHealth != maxHealth)
+        {
+            float healDone = Mathf.Min(heal, maxHealth - currentHealth);
+            currentHealth = Mathf.Min(currentHealth + heal, maxHealth);
+            onHealthChange?.Invoke(getHealthFraction(), Mathf.Min(currentHealth, healDone));
+        }
+    }
+
+    public float getHealth()
+    {
+        return currentHealth;
+    }
+}
